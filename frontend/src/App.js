3e29612ea6c42c6d4b0a9ca0f4fc27b1b25ee0c2
Stone Comstock <comstockstone@yahoo.com>
2023-02-10 18:38:56 +0700

    Basic routing set up with React Router Dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
 function App() {
   return (
-    <div>
-      My App
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
