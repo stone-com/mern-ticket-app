b6b4b9b21c112c207fc47e712f1627a499b6b675
Stone Comstock <comstockstone@yahoo.com>
2023-02-10 18:45:11 +0700

    Basic header/navbar set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
@@ -8,6 +9,7 @@ function App() {
     <>
       <Router>
         <div className='container'>
+          <Header />
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/login' element={<Login />} />
