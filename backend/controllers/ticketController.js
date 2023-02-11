fc5fcbea6b3cb6a989485b515ac348ee9a98c0ee
Stone Comstock <comstockstone@yahoo.com>
2023-02-11 19:32:09 +0700

    get tickets controller

--- backend/controllers/ticketController.js
+++ backend/controllers/ticketController.js
@@ -7,15 +7,24 @@ const Ticket = require('../models/ticketModel');
 // GET /api/tickets/
 // Protected route
 const getTickets = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: 'Get tickets' });
+  // Get user using the ID in JWT(being set in auth middleware)
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const tickets = await Ticket.find({ user: req.user.id });
+
+  res.status(200).json(tickets);
 });
 
 // create new ticket
 // POST /api/tickets/
 // Protected route
 const createTicket = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Create ticket' });
-  });
+  res.status(200).json({ message: 'Create ticket' });
+});
 
-
-  module.exports = {getTickets, createTicket}
+module.exports = { getTickets, createTicket };
