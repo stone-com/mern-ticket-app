9732bc3b60686f4d69d7eb8ec888f9429597c8ef
Stone Comstock <comstockstone@yahoo.com>
2023-02-12 17:29:13 +0700

    back button component

--- frontend/src/pages/NewTicket.jsx
+++ frontend/src/pages/NewTicket.jsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import { createTicket, reset } from '../features/tickets/ticketSlice';
 import Spinner from '../Components/Spinner';
+import BackButton from '../Components/BackButton';
 
 const NewTicket = () => {
   const { user } = useSelector((state) => state.auth);
@@ -40,6 +41,7 @@ const NewTicket = () => {
   }
   return (
     <>
+    <BackButton url = '/' />
       <section className='heading'>
         <h1>Create New Ticket</h1>
         <p>Please fill out the form below</p>
