c547c5de9444dadc948ecd3d658f3e8a0fae7e8e
Stone Comstock <comstockstone@yahoo.com>
2023-02-19 20:06:56 +0700

    backend to get notes and add notes

---
+++ backend/controllers/noteController.js
@@ -0,0 +1,63 @@
+const asyncHandler = require('express-async-handler');
+
+const Note = require('../models/noteModel');
+const User = require('../models/userModel');
+const Ticket = require('../models/ticketModel');
+
+// Get notes for a ticket
+// GET /api/tickets/:ticketId/notes
+// Protected route
+const getNotes = asyncHandler(async (req, res) => {
+  // Get user using the ID in JWT(being set in auth middleware)
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.ticketId);
+
+  if (ticket.user.toString() !== req.user.id) {
+    request.status(401);
+    throw new Error('User not authorized');
+  }
+
+  const notes = await Note.find({ ticket: req.params.ticketId });
+
+  res.status(200).json(notes);
+});
+
+// create a note for a ticket
+// POST /api/tickets/:ticketId/notes
+// Protected route
+const addNote = asyncHandler(async (req, res) => {
+  // Get user using the ID in JWT(being set in auth middleware)
+  const user = await User.findById(req.user.id);
+
+  if (!user) {
+    res.status(401);
+    throw new Error('User not found');
+  }
+
+  const ticket = await Ticket.findById(req.params.ticketId);
+
+  if (ticket.user.toString() !== req.user.id) {
+    request.status(401);
+    throw new Error('User not authorized');
+  }
+
+  const note = await Note.create({
+    text: req.body.text,
+    isStaff: false,
+    ticket: req.params.ticketId,
+    user: req.user.id,
+  });
+
+  res.status(200).json(notes);
+});
+
+module.exports = {
+  getNotes,
+  addNote,
+};
--- backend/routes/ticketRoutes.js
+++ backend/routes/ticketRoutes.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { protect } = require('../middleware/authMiddleware');
+
+const noteRouter = require('./noteRoutes');
+
+// Reroute into note router
+router.use('/:ticketId/notes', noteRouter);
+
 const {
   getTickets,
   createTicket,
