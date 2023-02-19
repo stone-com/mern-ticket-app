d65714628a5d3b9368c7dbad461b736c85347404
Stone Comstock <comstockstone@yahoo.com>
2023-02-19 19:23:42 +0700

    JSX for displaying ticket info done, need to add notes fuinctionality next

--- frontend/src/pages/Ticket.jsx
+++ frontend/src/pages/Ticket.jsx
@@ -1,6 +1,54 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import { getTicket, reset } from '../features/tickets/ticketSlice';
+import { useParams } from 'react-router-dom';
+import BackButton from '../Components/BackButton';
+import Spinner from '../Components/Spinner';
+
 const Ticket = () => {
+  const { ticket, isLoading, isSuccess, isError, message } = useSelector(
+    (state) => state.ticket
+  );
+
+  const dispatch = useDispatch();
+  const { ticketId } = useParams();
+
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
+    dispatch(getTicket(ticketId));
+    // eslint-disable-next-line
+  }, [isError, message, ticketId]);
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  if (isError) {
+    return <h3>Something went wrong!</h3>;
+  }
   return (
-    <div>Ticket</div>
-  )
-}
-export default Ticket
+    <div className='ticket-page'>
+      <header className='ticket-header'>
+        <BackButton url='/tickets' />
+        <h2>
+          Ticket ID: {ticket._id}
+          <span className={`status status-${ticket.status}`}>
+            {ticket.status}
+          </span>
+        </h2>
+        <h3>
+          Date Submitted: {new Date(ticket.createdAt).toLocaleString('en-Us')}
+        </h3>
+        <hr />
+        <div className='ticket-desc'>
+          <h3>Description of Issue</h3>
+          <p>{ticket.description}</p>
+        </div>
+      </header>
+    </div>
+  );
+};
+export default Ticket;
